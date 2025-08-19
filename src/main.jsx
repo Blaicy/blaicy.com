97bce679066bb0c47a8f584d85fd8c41e4916f15
Blaicy <blaicymokaya@gmail.com>
2025-08-19 20:13:02 +0300

    Additions

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <HashRouter>
-        <App />
+   <HashRouter >
+      <App />
     </HashRouter>
   </React.StrictMode>
 )
